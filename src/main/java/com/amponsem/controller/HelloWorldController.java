c580f2cd99d7439782d75f3c53e24f3902091b7c
amponsemmichael <amponsem_michael@yahoo.com>
2024-07-27 08:25:32 +0000

    built first controller

---
+++ src/main/java/com/amponsem/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.amponsem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    //return a statement
+    @GetMapping("/test1")
+    @ResponseBody
+    public String helloWorld() {
+        return "Welcome to 2024 Olympics Games";
+    }
+}
--- src/main/java/com/amponsem/webdevinspringboot/WebdevinspringbootApplication.java
+++ src/main/java/com/amponsem/webdevinspringboot/WebdevinspringbootApplication.java
@@ -2,8 +2,10 @@ package com.amponsem.webdevinspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.amponsem")
 public class WebdevinspringbootApplication {
 
 	public static void main(String[] args) {
