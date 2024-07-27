bc6ecead5774ddfa65a0a2f693acff22c0f0dcaf
amponsemmichael <amponsem_michael@yahoo.com>
2024-07-27 07:58:24 +0000

    initial project setup

---
+++ src/main/java/com/amponsem/webdevinspringboot/WebdevinspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.amponsem.webdevinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevinspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevinspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amponsem/webdevinspringboot/WebdevinspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amponsem.webdevinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdevinspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
